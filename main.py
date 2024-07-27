02cc1c7833f9b16648fec83c7c13f142c2571bdf
Satendra Singh <satendrasinghcse@gmail.com>
2024-07-27 05:43:15 +0000

    notebooks added

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.component.data_ingestion import data_lodder
+from src.component.data_transformation import data_transform
+from src.component.trainer import model_trainer
+#from.src.component.recommend import recommend
+
+data_lodder()
+data_transform()
+model_trainer()
+
